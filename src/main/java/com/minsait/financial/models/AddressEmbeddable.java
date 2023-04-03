d992b3b6dbd33736d41d35106effd3fd3aef3f3f
José Roberto Jr <joserobertodsj@gmail.com>
2023-04-03 20:53:44 -0300

    fix: add annotations in AddressEmbeddable

--- src/main/java/com/minsait/financial/models/AddressEmbeddable.java
+++ src/main/java/com/minsait/financial/models/AddressEmbeddable.java
@@ -1,6 +1,7 @@
 package com.minsait.financial.models;
 
 import jakarta.persistence.Embeddable;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -12,8 +13,10 @@ import lombok.Setter;
 @Setter
 @Getter
 public class AddressEmbeddable {
-
+    @NotBlank(message = "Preenchimento obrigatório.")
     private String street;
+    @NotBlank(message = "Preenchimento obrigatório.")
     private String number;
+    @NotBlank(message = "Preenchimento obrigatório.")
     private String zipCode;
 }
