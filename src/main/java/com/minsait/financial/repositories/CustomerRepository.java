0a7cdca377a51ffcc77116b6c060317c389a341a
José Roberto Jr <joserobertodsj@gmail.com>
2023-04-03 14:16:38 -0300

    feat: create CustomerRepository interface

---
+++ src/main/java/com/minsait/financial/repositories/CustomerRepository.java
@@ -0,0 +1,9 @@
+package com.minsait.financial.repositories;
+
+import com.minsait.financial.models.CustomerModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CustomerRepository extends JpaRepository<CustomerModel, Long> {
+}
