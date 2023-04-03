184281e09a9f44f694a504138d6b8482aad9613e
José Roberto Jr <joserobertodsj@gmail.com>
2023-04-03 12:18:36 -0300

    feat: create address

---
+++ src/main/java/com/minsait/financial/AddressModel.java
@@ -0,0 +1,22 @@
+package com.minsait.financial;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "tb_address")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class AddressModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String street;
+    private String number;
+    private String zipCode;
+}
