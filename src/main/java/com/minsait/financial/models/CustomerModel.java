821c7e2966448a15a4872c40176867f3c457389f
José Roberto Jr <joserobertodsj@gmail.com>
2023-04-03 12:11:11 -0300

    feat: create customer

---
+++ src/main/java/com/minsait/financial/models/CustomerModel.java
@@ -0,0 +1,27 @@
+package com.minsait.financial.models;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "tb_customer", uniqueConstraints={@UniqueConstraint(columnNames={"documentNumber"})})
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class CustomerModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String documentNumber;
+    private String phoneNumber;
+    private BigDecimal monthlyIncome;
+
+}
