2c90f54555961defaac68eef52a1d571ad40bae3
José Roberto Jr <joserobertodsj@gmail.com>
2023-04-03 23:16:23 -0300

    feat: create method in CustomerRepository interface

--- src/main/java/com/minsait/financial/repositories/CustomerRepository.java
+++ src/main/java/com/minsait/financial/repositories/CustomerRepository.java
@@ -4,6 +4,10 @@ import com.minsait.financial.models.CustomerModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface CustomerRepository extends JpaRepository<CustomerModel, Long> {
+    Optional<CustomerModel>  findByDocumentNumber(String documentNumber);
+
 }
