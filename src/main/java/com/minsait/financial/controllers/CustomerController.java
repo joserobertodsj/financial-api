632b977394fdc73c13839b28e3537d2ca72aedb7
José Roberto Jr <joserobertodsj@gmail.com>
2023-04-03 23:19:55 -0300

    feat: create endpoints

--- src/main/java/com/minsait/financial/controllers/CustomerController.java
+++ src/main/java/com/minsait/financial/controllers/CustomerController.java
@@ -8,10 +8,9 @@ import lombok.AllArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/customers")
@@ -25,6 +24,23 @@ public class CustomerController {
     @PostMapping
     public ResponseEntity<CustomerResponseDto> createCustomer(@RequestBody @Valid CustomerRequestDto customerRequestDto){
         return ResponseEntity.status(HttpStatus.CREATED).body(customerService.createCustomer(customerRequestDto));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<CustomerResponseDto>> getAllCustomers(){
+        return ResponseEntity.status(HttpStatus.OK).body(customerService.getAllCustomers());
+    }
+
+    @GetMapping("/{documentNumber}")
+    public ResponseEntity<CustomerResponseDto> getCustomerByDocumentNumber(@PathVariable @Valid String documentNumber){
+        return ResponseEntity.status(HttpStatus.OK).body(customerService.GetByDocumentNumber(documentNumber));
+    }
+
+    @DeleteMapping("/{documentNumber}")
+    public ResponseEntity<Void> deleteCustomerByDocumentNumber(@PathVariable String documentNumber){
+        customerService.deleteByDocumentNumber(documentNumber);
+         return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+
     }
 
 }
