19bab12b13cdc994d737a57a67cd28c5118140c1
José Roberto Jr <joserobertodsj@gmail.com>
2023-04-03 23:15:23 -0300

    fix: change method names

--- src/main/java/com/minsait/financial/services/CustomerService.java
+++ src/main/java/com/minsait/financial/services/CustomerService.java
@@ -2,14 +2,16 @@ package com.minsait.financial.services;
 
 import com.minsait.financial.models.dtos.requests.CustomerRequestDto;
 import com.minsait.financial.models.dtos.responses.CustomerResponseDto;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public interface CustomerService {
     CustomerResponseDto createCustomer(CustomerRequestDto customerRequestDto);
     List<CustomerResponseDto> getAllCustomers();
-    CustomerResponseDto getCustomerByDocumentNumber(String documentoNumber);
-    void deleteCustomerByDocumentNumber(String documentNumber);
+    CustomerResponseDto GetByDocumentNumber(String documentNumber);
+    void deleteByDocumentNumber(String documentNumber);
     CustomerResponseDto updateCustomer(String documentNumber, CustomerRequestDto customerRequestDto);
 
 
