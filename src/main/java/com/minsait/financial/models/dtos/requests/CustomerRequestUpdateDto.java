2a4fb07930877f9572ff6506c685f57e7f42a5fc
José Roberto Jr <joserobertodsj@gmail.com>
2023-04-04 11:39:12 -0300

    feat: create CustomerRequestUpdateDto

---
+++ src/main/java/com/minsait/financial/models/dtos/requests/CustomerRequestUpdateDto.java
@@ -0,0 +1,31 @@
+package com.minsait.financial.models.dtos.requests;
+
+import com.minsait.financial.models.AddressEmbeddable;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.Digits;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class CustomerRequestUpdateDto {
+    @NotBlank(message = "Preenchimento obrigatório.")
+    private String name;
+    @NotBlank(message = "Preenchimento obrigatório.")
+    private String phoneNumber;
+    @DecimalMin(value = "0.0" , inclusive = false)
+    @Digits(integer = 20, fraction = 2)
+    @NotNull(message = "Preenchimento obrigatório." )
+    private BigDecimal monthlyIncome;
+    @Valid
+    private AddressEmbeddable address;
+}
