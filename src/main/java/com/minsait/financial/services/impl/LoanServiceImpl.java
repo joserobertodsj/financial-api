8c1b629517e3fb9a9ac39c3e1e538ea7b6458efb
José Roberto Jr <joserobertodsj@gmail.com>
2023-04-09 22:48:21 -0300

    feat: implements methods in LoanServiceImpl

--- src/main/java/com/minsait/financial/services/impl/LoanServiceImpl.java
+++ src/main/java/com/minsait/financial/services/impl/LoanServiceImpl.java
@@ -6,6 +6,7 @@ import com.minsait.financial.models.CustomerModel;
 import com.minsait.financial.models.LoanModel;
 import com.minsait.financial.models.dtos.requests.loan.LoanRequestDto;
 import com.minsait.financial.models.dtos.responses.loan.LoanResponseDto;
+import com.minsait.financial.repositories.CustomerRepository;
 import com.minsait.financial.repositories.LoanRepository;
 import com.minsait.financial.services.LoanService;
 import com.minsait.financial.strategies.loans.BronzeRelationshipLoan;
@@ -17,6 +18,7 @@ import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 @Service
 @AllArgsConstructor
@@ -24,6 +26,7 @@ public class LoanServiceImpl implements LoanService {
 
     private final LoanRepository loanRepository;
     private final CustomerServiceImpl customerService;
+    private final CustomerRepository customerRepository;
     private final ModelMapper modelMapper;
 
     @Override
@@ -31,7 +34,8 @@ public class LoanServiceImpl implements LoanService {
         CustomerModel customerModel = modelMapper.map(customerService.GetByDocumentNumber(documentNumber), CustomerModel.class);
         var loan = modelMapper.map(loanRequestDto, LoanModel.class);
         loan.setCustomerModel(customerModel);
-        if (canGetNewLoan(customerModel)){
+        loan.setCustomerDocumentNumber(documentNumber);
+        if (canGetNewLoan(customerModel, loan)){
             BigDecimal loanFinalValue = calculateFinalValue(loan);
             loan.setFinalValue(loanFinalValue);
             loanRepository.save(loan);
@@ -42,11 +46,24 @@ public class LoanServiceImpl implements LoanService {
     }
 
     @Override
-    public LoanResponseDto getLoanById(String documentNumber, Long id) {
+    public LoanResponseDto getLoan(String documentNumber, Long id) {
         customerService.GetByDocumentNumber(documentNumber);
         return modelMapper.map(loanRepository.findById(id)
                 .orElseThrow(() -> new LoanNotFoundException("Empréstimo não encontrado.")), LoanResponseDto.class);
     }
+
+    @Override
+    public void delete(String documentNumber, Long id) {
+        getLoan(documentNumber, id);
+        loanRepository.deleteById(id);
+    }
+
+    @Override
+    public List<LoanResponseDto> getAllLoans(String documentNumber) {
+        CustomerModel customerModel = modelMapper.map(customerService.GetByDocumentNumber(documentNumber), CustomerModel.class);
+       return customerModel.getLoans().stream().map(loan ->modelMapper.map(loan, LoanResponseDto.class)).toList();
+    }
+
 
     private BigDecimal calculateFinalValue (LoanModel loan){
         LoanStrategy loanStrategy;
@@ -62,12 +79,12 @@ public class LoanServiceImpl implements LoanService {
         return loanStrategy.calculateFinalValue(loan);
     }
 
-    private Boolean canGetNewLoan(CustomerModel customer){
+    private Boolean canGetNewLoan(CustomerModel customer, LoanModel loan){
         BigDecimal total = new BigDecimal(0);
         for (LoanModel l : customer.getLoans()) {
             total = total.add(l.getInitialValue());
         }
-        if (total.compareTo(customer.getMonthlyIncome().multiply(new BigDecimal(10))) == 1){
+        if (total.add(loan.getInitialValue()).compareTo(customer.getMonthlyIncome().multiply(new BigDecimal(10)))  == 1){
             return false;
         }
         return true;
