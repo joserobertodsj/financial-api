c89f7e8393af05a720476989186fad27bb3e6406
José Roberto Jr <joserobertodsj@gmail.com>
2023-04-09 22:45:56 -0300

    fix: add annotation

--- src/main/java/com/minsait/financial/repositories/LoanRepository.java
+++ src/main/java/com/minsait/financial/repositories/LoanRepository.java
@@ -2,6 +2,9 @@ package com.minsait.financial.repositories;
 
 import com.minsait.financial.models.LoanModel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface LoanRepository extends JpaRepository<LoanModel, Long> {
+
 }
