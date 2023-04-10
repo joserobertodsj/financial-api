5de119a9599006a25760767f337f9b8cad20ef59
José Roberto Jr <joserobertodsj@gmail.com>
2023-04-09 22:47:49 -0300

    feat: create methods in LoanService

--- src/main/java/com/minsait/financial/services/LoanService.java
+++ src/main/java/com/minsait/financial/services/LoanService.java
@@ -3,7 +3,12 @@ package com.minsait.financial.services;
 import com.minsait.financial.models.dtos.requests.loan.LoanRequestDto;
 import com.minsait.financial.models.dtos.responses.loan.LoanResponseDto;
 
+import java.util.List;
+
 public interface LoanService {
     LoanResponseDto createLoan (String documentNumber, LoanRequestDto loanRequestDto);
-    LoanResponseDto getLoanById(String documentNumber, Long id);
+    LoanResponseDto getLoan(String documentNumber, Long id);
+    void delete(String documentNumber, Long id);
+
+    List<LoanResponseDto> getAllLoans(String documentNumber);
 }
